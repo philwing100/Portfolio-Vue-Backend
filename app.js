63ea1c46145524b9e9352556a3e7c079238c5393
Phil Wing <philwing100@gmail>
2025-02-07 14:31:11 -0600

    updated frontend link

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ require('dotenv').config();
 
 const app = express();
 const port = process.env.FRONTENDPORT || 3000;
-const frontendPath =  port === 3000 ? "http://localhost:8080" : "https://phillip-ring.vercel.app/";
+const frontendPath =  port === 3000 ? "http://localhost:8080" : "https://phillip-ring.vercel.app";
 
 // Session store options
 const options = {
