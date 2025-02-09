a24da3ecaabfbf6fb7901afb001ca670dab0eff6
Phil Wing <philwing100@gmail>
2025-02-08 19:09:07 -0600

    Revert test of lax

--- app.js
+++ app.js
@@ -50,9 +50,10 @@ app.use(session({
     maxAge: 1000 * 60 * 120, // Set cookie lifespan (30 minutes)
     httpOnly: true,
     secure: process.env.NODE_ENV === "production", // ✅ Secure only in production
-    sameSite: process.env.NODE_ENV === "production" ? "lax" : "lax",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
   },
 }));
+console.log(process.env.NODE_ENV);
 
 // Initialize Passport for authentication
 app.use(passport.initialize());
